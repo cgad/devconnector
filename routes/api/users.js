6069ed6c95558234cbcf1d5ede383269a1102203
cgad <35946778+cgad@users.noreply.github.com>
2018-11-24 13:14:28 -0500

    Registration validation rules for email, password, password2

--- routes/api/users.js
+++ routes/api/users.js
@@ -43,7 +43,8 @@ router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }) // Pass in object to find email that matches req.body.email
     .then(user => {
       if (user) {
-        return res.status(400).json({ email: "Email already exists" });
+        errors.email = "Email already exists";
+        return res.status(400).json(errors);
       } else {
         // Create new user
         const avatar = gravatar.url(req.body.email, {
--- validation/register.js
+++ validation/register.js
@@ -6,9 +6,47 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
+  // Validator's isEmpty method can only handle strings
+  // If data.name is not empty, set it equal to data.name. Else, set it to empty string
+  // Using the isEmpty method we created which can deal with things other than strings
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : ""; // Confirm password
+
   // Registration name at least 2 characters, no more than 30
   if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
     errors.name = "Name must be between 2 and 30 characters";
+  }
+
+  // Validator's isEmpty method can only handle strings, so set to empty string above if name is empty
+  if (Validator.isEmpty(data.name)) {
+    errors.name = "Name field is required";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  // Validator's isEmail method
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
   }
 
   // Pull these into every route that takes in data
