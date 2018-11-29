06796d8c045d894c7873337d7d84d8b446d50586
cgad <35946778+cgad@users.noreply.github.com>
2018-11-28 20:37:11 -0700

    Routes to get all profiles, get profile by handle, get profile by ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -14,12 +14,6 @@ const Profile = require("../../models/Profile");
 // Load User model
 const User = require("../../models/User");
 
-// Don't need /api/profile first because we already did that in server.js
-// @route  GET api/profiles/test
-// @description  Tests profiles route
-// @access  Public
-router.get("/test", (req, res) => res.json({ msg: "Profiles works" }));
-
 // @route  GET api/profile
 // @description  Get current user's profile
 // @access  Private
@@ -43,9 +37,70 @@ router.get(
   }
 );
 
+// @route  GET api/profile/all
+// @description  Get all profiles
+// @access  Public
+router.get("/all", (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There is no profile for this user";
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: "There are no profiles" }));
+});
+
+// @route  GET api/profile/handle/:handle
+// @description  Get profile by handle
+// @access  Public
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
+
+// @route  GET api/profile/user/:user_id
+// @description  Get profile by user ID
+// @access  Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "There is no profile for this user" })
+    );
+});
+
 // @route  POST api/profile
 // @description  Create or Edit user profile
-// @access  Private
+// @access  Private (so use passport middleware)
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
