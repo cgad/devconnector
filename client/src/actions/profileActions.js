b48e5afa257f66b44f319ebb0a8b511c886bc4e0
cgad <35946778+cgad@users.noreply.github.com>
2018-12-13 11:31:06 -0700

    Spinner component for loading, write Dashboard connect jsx and check if current user has profile data

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-import setAuthToken from "../utils/setAuthToken";
-import jwt_decode from "jwt-decode";
 
-import { GET_PROFILE, PROFILE_LOADING, GET_ERRORS, CLEAR_CURRENT_PROFILE } from "./types";
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
