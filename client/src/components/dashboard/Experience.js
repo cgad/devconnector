f847a28ebcf96b611c37e7440980481e1039b61e
cgad <35946778+cgad@users.noreply.github.com>
2018-12-17 13:37:11 -0700

    Typo

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -17,9 +17,9 @@ class Experience extends Component {
         <td>
           <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{" "}
           {exp.to === null ? (
-            "\Now"
+            "Now"
           ) : (
-            <Moment format="YYYY/MM/DD">{" "}{exp.to}</Moment>
+            <Moment format="YYYY/MM/DD"> {exp.to}</Moment>
           )}
         </td>
         <td>
