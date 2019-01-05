891dd69ebab88e578fd319c6056ca6069a4b166a
cgad <35946778+cgad@users.noreply.github.com>
2019-01-04 17:01:36 -0700

    Prepared for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport"); // Auth module
+const path = require("path");
 
 // Point certain URLs to these files
 // For example, if the route is /api/users, connect to this file
@@ -32,10 +33,24 @@ app.use(passport.initialize());
 // Passport Config
 require("./config/passport")(passport);
 
-// use routes
+// Use routes
+// If none of these are hit, use build folder index.html
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets if in production
+// Test to see if in production (on heroku ie server)
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  // Create route- get anything that isn't an api route
+  app.get("*", (req, res) =>
+    // Send file using path module (part of NodeJS)
+    // in sendFile(), pass in current directory and go into client, build, load index.html
+    res.sendFile(path.resolve(__dirname, "client", "build", "index,html"))
+  );
+}
 
 // process.env.PORT necessary to deploy to heroku
 const port = process.env.PORT || 5000;
