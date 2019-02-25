5b6f5d57df0ef09173e5652afc936b050243deab
cgad <35946778+cgad@users.noreply.github.com>
2019-02-25 15:17:50 -0700

    Heroku deployment troubleshoot

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-// Server static assets if in production
+// Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
   app.get("*", (req, res) => {
     // Load index react file for any path hit here
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
